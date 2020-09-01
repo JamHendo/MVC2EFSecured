61b3e7c37c34d37f6ef042ee75c8b00d34d74130
James Henderson <jameshenderson_itpro@outlook.com>
2020-09-01 10:26:34 -0500

    Added the ContactViewModel

--- MVC2EFSecured.UI.MVC/Controllers/HomeController.cs
+++ MVC2EFSecured.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using MVC2EFSecured.UI.MVC.Models;
+using System.Net.Mail; //mail
+using System.Net; //mail
+using System; // Exception handling
 
 namespace MVC2EFSecured.UI.MVC.Controllers
 {
@@ -26,5 +30,57 @@ namespace MVC2EFSecured.UI.MVC.Controllers
 
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            //  Check that the cvm data just passed into the method is a valid ContactViewModel
+            if (ModelState.IsValid)
+            {
+                string message = $"You have recieved an email from {cvm.Name} with a subject of {cvm.Subject}. " +
+                    $"Please respond to {cvm.Email} with your response to the following message:<br/>{cvm.Message}";
+
+                MailMessage mm = new MailMessage("no-reply@thehendersoncollective.net", "no-reply@thehendersoncollective.net", cvm.Subject, message);
+
+                //Mail message Properties
+
+                //Allow HTML formatting in the email
+                mm.IsBodyHtml = true;
+
+                mm.Priority = MailPriority.High;
+
+                //Respnd to the sender and not the admin @ your site URL.com
+                mm.ReplyToList.Add(cvm.Email);
+                mm.CC.Add("yourCCperson@yoursite.com");
+
+                //SmtpClient - This is the info from the host that allows this message to be sent
+                SmtpClient client = new SmtpClient("mail.yourmaildomain.com");
+
+                client.Credentials = new NetworkCredential("admin@yoursite.com", "Your password"); //Clear text password is not best practice
+                //set the port
+                client.Port = 8889;
+                //try to send the email
+                try
+                {//attempt to send
+                    client.Send(mm);
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"We're sorry your resquest could not be completed at this time. Please try again later. Error message:<br/> {ex.StackTrace}";
+                    return View(cvm);
+                }
+                
+
+
+                return View();
+
+            }else // The ModelState is NOT valid
+            {                // Send back the form with the completed inputs so the user can try again and not have to re-type everything
+
+                return View(cvm);
+            }
+        }
+            
     }
 }
